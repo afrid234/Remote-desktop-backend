4af8c279cc7b5bb78aeaa2bebc40166242007e7b
Afrid Faruk <121674062+afrid234@users.noreply.github.com>
2025-02-04 15:10:25 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-                                                                                                                                                                                                                                                                                                                                                  server.js                                                                                                                                                                                                                                                                                                                                                                 const express = require('express');
+const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const fs = require('fs');
@@ -114,11 +114,4 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-
-
-
-
-
-
 
